1ca6a48c54d276b70efe9255cc6c24c8a164c345
shobhitkothiyal2-lang <shobhitkothiyal2@gmail.com>
2026-04-15 19:45:46 +0530

    changed whole project and solved errors

--- routes/stockNotification.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import stockNotificationController from "../controller/stockNotification.controller.js";
-
-const router = express.Router();
-
-router.post("/", stockNotificationController.createNotification);
-
-export default router;
